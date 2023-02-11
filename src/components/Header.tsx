35fde6bb4073a0b6dac02e492c06b134ebf91373
Aen Jojo <josuafernando999@gmail.com>
2023-02-11 19:01:41 +0700

    feat: add header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+export function Header() {
+	return (
+		<header className='bg-black text-yellow-300'>
+			<section className='container mx-auto p-2'>
+				<div className='mx-auto w-fit text-2xl'>
+					CypherText
+				</div>
+			</section>
+		</header>
+	);
+}
